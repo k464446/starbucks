90963d3ba2c1df4a70678714e8200afef02120e4
k464446 <k464446@naver.com>
2021-04-24 19:15:02 +0900

    로그인 페이지 완성

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-// 변수를 찾아야 한다, 돋보기 아이콘 누르면 인풋 요소 늘어남
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-searchEl.addEventListener('click', function () {
-    searchInputEl.focus();
-});
-
-//  인풋 요소에 입력하는 통합 검색
-searchInputEl.addEventListener('focus', function () {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-//  인풋 요소에 입력하는 통합 검색 제거
-searchInputEl.addEventListener('blur', function () {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
- 
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -153,8 +133,4 @@ spyEls.forEach(function (spyEl) {
         .setClassToggle(spyEl, 'show')
         .addTo(new ScrollMagic.Controller());
 });
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //2021
-
 
